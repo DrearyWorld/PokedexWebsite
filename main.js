b1684c1324b403c2f1691cd88a3d19ec8b756858
Your Name <chungw2001@gmail.com>
2022-08-02 16:45:41 -0400

    ApiCall

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("Hello World")
+console.log("Hello World");
+
+const fetchPokemon = () => {
+    const url = "https://pokeapi.co/api/v2/pokemon/1"
+    fetch(url)
+        .then((res) => {
+            return res.json();
+        })
+        .then( (data) => {
+            console.log(data);
+            const pokemon = {
+                name: data.name,
+                id: data.id,
+                image: data.sprites['front_default'],
+                type: data.types
+            };
+            console.log(pokemon);
+        });
+};
+
+fetchPokemon();
