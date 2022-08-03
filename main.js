13163eb64527391a85b8a61fb56a0c896c364d6f
Your Name <chungw2001@gmail.com>
2022-08-02 20:10:43 -0400

    comments and loop

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
 console.log("Hello World");
 
 const fetchPokemon = () => {
-    const url = "https://pokeapi.co/api/v2/pokemon/1"
-    fetch(url)
-        .then((res) => {
-            return res.json();
-        })
-        .then( (data) => {
-            console.log(data);
-            const pokemon = {
-                name: data.name,
-                id: data.id,
-                image: data.sprites['front_default'],
-                type: data.types
-            };
-            console.log(pokemon);
-        });
+    for (let i = 1; i <= 151; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}` // ${i} iterates through 1-151 and makes the api call for each 1-151 pokemon
+        fetch(url) // fetch information 
+            .then((res) => { //returns a promise, in that promise define callback
+                return res.json(); //after make request and get response, converts response to json
+            })
+            .then( (data) => { //returns another promise to get our data and then print out our data below
+                console.log(data);
+                const pokemon = { //creates pokemon object with the specific data below
+                    name: data.name, //name of pokemon
+                    id: data.id, //id of pokemon
+                    image: data.sprites['front_default'], //default sprite
+                    type: data.types.map(type => type.type.name).join(",") //COME BACK TO THIS MAD CONFUSED
+                };
+                console.log(pokemon);
+            });
+    }
 };
 
 fetchPokemon();
+
