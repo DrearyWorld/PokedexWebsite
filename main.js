539d9011eacd722b4a6dd32e7cbdc8580a2df580
Your Name <chungw2001@gmail.com>
2022-08-03 21:46:34 -0400

    dataToHTML

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+const pokemonCard = document.querySelector(".pokemon-card");
 
 const fetchPokemon = () => {
     for (let i = 1; i <= 151; i++) {
@@ -8,17 +8,41 @@ const fetchPokemon = () => {
                 return res.json(); //after make request and get response, converts response to json
             })
             .then( (data) => { //returns another promise to get our data and then print out our data below
-                console.log(data);
                 const pokemon = { //creates pokemon object with the specific data below
                     name: data.name, //name of pokemon
                     id: data.id, //id of pokemon
                     image: data.sprites['front_default'], //default sprite
-                    type: data.types.map(type => type.type.name).join(",") //COME BACK TO THIS MAD CONFUSED
+                    type: data.types.map(type => type.type.name).join(",") //returns the types from array to string
                 };
                 console.log(pokemon);
+                createPokemonCard(pokemon);
             });
     }
 };
+
+const createPokemonCard = (some_pokemon) => {
+
+    const card = document.createElement("div");
+    card.classList.add("pokemon-block")
+
+    const spirteContainer = document.createElement("div");
+    spirteContainer.classList.add("img-container");
+
+    const sprite = document.createElement("img");
+    sprite.src = some_pokemon.image;
+
+    spirteContainer.appendChild(sprite);
+
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.textContent = some_pokemon.name;
+
+    card.appendChild(spirteContainer);
+    card.appendChild(name);
+
+    pokemonCard.appendChild(card);
+};
+
 
 fetchPokemon();
 
