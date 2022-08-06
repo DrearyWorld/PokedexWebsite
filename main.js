325574266a0fe418f3f544c4ade96c07a695e94c
Your Name <chungw2001@gmail.com>
2022-08-05 20:32:05 -0400

    grids

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const pokemonCard = document.querySelector(".pokemon-card");
+const searchInput = document.querySelector("[data-search");
+
+searchInput.addEventListener("input", (e) => {
+    searchString = e.target.value;
+});
+
 
 const fetchPokemon = () => {
-
     const promises = [];
     for (let i = 1; i <= 151; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}` // ${i} iterates through 1-151 and makes the api call for each 1-151 pokemon
@@ -10,13 +15,13 @@ const fetchPokemon = () => {
         //returns a promise, in that promise define callback
         //after make request and get response, converts response to json
     }
-    
+
     Promise.all(promises).then( (results) => {
         const pokemon = results.map((data) => ({
             name: data.name, //name of pokemon
             id: data.id, //id of pokemon
             picture: data.sprites['front_default'], //default sprite
-            type: data.types.map(type => type.type.name).join(",") //returns the types from array to string
+            type: data.types.map(type => type.type.name).join(" / ") //returns the types from array to string
         }));
         createPokemonCard(pokemon);
     });
@@ -36,16 +41,23 @@ const createPokemonCard = (some_pokemon) => {
         spirteContainer.appendChild(sprite);
 
         const name = document.createElement("p");
-        name.classList.add("name");
-        name.textContent = some_pokemon[j].name;
+        name.classList.add("poke_name");
+        name.textContent = some_pokemon[j].id+ ". " + (some_pokemon[j].name).charAt(0).toUpperCase() + (some_pokemon[j].name).slice(1);
+        if (j == 121) {
+            name.textContent = some_pokemon[j].id+ ". " + (some_pokemon[j].name.charAt(0).toUpperCase() + some_pokemon[j].name.slice(1,3)) + some_pokemon[j].name.charAt(3).toUpperCase() + some_pokemon[j].name.slice(4);
+        }; //Mr-mime -> Mr-Mime; 
+
+        const type = document.createElement("p");
+        type.classList.add("poke_type");
+        type.textContent = "Type: " + some_pokemon[j].type.toUpperCase();
 
         card.appendChild(spirteContainer);
         card.appendChild(name);
+        card.appendChild(type);
 
         pokemonCard.appendChild(card);
     }
 };
-
 
 fetchPokemon();
 
